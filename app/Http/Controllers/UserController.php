4a1b0f69754435813031d25bc88253ba9780cdc0
lennne <kobiansah@gmail.com>
2024-10-13 16:34:14 -0230

    create routes for logging in and out, creating new tables in mysql db, creating posts for specific user and storing them in db

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,17 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
+
 {
+
+
+    public function login(Request $request){
+        $incomingFields = $request->validate([
+            'loginname' => 'required',
+            'loginpassword' => 'required'
+        ]); 
+  
+    if(auth()->attempt(['name'=> $incomingFields['loginname'],'password'=>$incomingFields['loginpassword']])){
+        $request->session()->regenerate();
+    } 
+
+    return redirect('/');
+    }
+
+    public function logout(){
+        auth()->logout();
+        return redirect('/');
+    }
+
     //
     public function register(Request $request){
         $incomingFields = $request->validate([
-            'name' => ['required','min:3','max:10'],
-            'email' => ['required','email'],
+            'name' => ['required','min:3','max:10',Rule::unique('users','name')],
+            'email' => ['required','email',Rule::unique('users','email')],
             'password' => ['required','min:3','max:300']
         ]);
-        return "hello from our controller by this time";
+        echo "hello";
+    $incomingFields['password'] = bcrypt($incomingFields['password']);
+    $user = User::create($incomingFields);
+    print("get out");
+    auth()->login($user);
+        return redirect('/');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,23 @@
     <title>Document</title>
 </head>
 <body>
+    @auth
+    <p>Congrats you're logged in</p>
+    <form action="/logout" method="POST">
+        @csrf
+        <button>Log Out</button>
+    </form>
+    
+    <div style="border: 3px solid black;"> 
+        <h1>POSTS</h1>
+        <form action="/create-post" method="POST">
+        @csrf
+        <input type="text" name="title" placeholder="post title ">
+        <textarea name="body" placeholder="body content...."></textarea>
+        <button>Save Post</button>
+        </form>
+    </div>
+    @else
     <div style="border: 3px solid black;">
         <h2>Register</h2>
         
@@ -17,5 +34,17 @@
             <button>Register</button>
         </form>
     </div>
+    <div style="border: 3px solid black;">
+        <h2>Login</h2>
+        <form action="/login" method="POST">
+        @csrf
+        <input type="text" name="loginname" placeholder="name">
+        <input type="password" name="loginpassword" placeholder="password">
+        <button>Login</button>
+        </form>
+    </div>
+    @endauth
+
+
 </body>
 </html>
